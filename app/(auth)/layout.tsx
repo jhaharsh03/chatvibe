0cc2ba2fbf40f8eb08fdbfad0cb7864a2a79d554
jhaharsh03 <harshj92002@gmail.com>
2024-04-01 22:23:51 +0530

    adding all the files

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import type { Metadata } from "next";
+import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+import { dark } from "@clerk/themes";
+
+export const metadata: Metadata = {
+    title: 'Chatvibe',
+    description: 'A Social Media App'
+}
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+    children 
+} : {
+    children : React.ReactNode 
+}) {
+    return(
+        <ClerkProvider
+            appearance={{
+                baseTheme: dark,
+            }}
+        >
+            <html lang='en'>
+                <body className={`${inter.className} bg-dark-1`}>
+                    <div className="w-full flex justify-center items-center min-h-screen">
+                        { children }
+                    </div> 
+                </body>
+            </html>
+        </ClerkProvider>
+    )
+}
